17ccf2b5968afacce628d272f2fb5731e4c9c779
Shoki-darou <shoki.rugby0115@gmail.com>
2024-03-20 14:00:00 +0900

    deviseの実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year - (birthday.to_date.change(year: now.year) > now ? 1 : 0)
+  end
+
+
+end
